de5761facec95424f40443732119df61a95fc29a
duaal <duaalkhair@gmail.com>
2022-10-27 16:40:50 +0300

    Initial Commit

---
+++ test3/test3App.swift
@@ -0,0 +1,17 @@
+//
+//  test3App.swift
+//  test3
+//
+//  Created by duaa mohammed on 27/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct test3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
